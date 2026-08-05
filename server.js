11954d9538898a6cd2a64cb46802746c62d1f739
Ingrid Rangel Hernandez <rang0128@algonquinlive.com>
2026-08-04 23:10:10 -0400

    MongoDB active

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
+// Express
 const express = require("express");
 const app = express();
-
-// Serve static files from the public dir
 app.use(express.static("public"));
-
 // Start the web server
 app.listen(5500, function() {
    console.log("Listening on port 5500...");
@@ -11,6 +9,15 @@ app.listen(5500, function() {
 
 app.set("views", "views");
 app.set("view engine", "pug");
+
+
+// MongoDB
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://ingridrh2005_db_user:e8rDFKeaMsecu6Ou@a2.ko4lkxu.mongodb.net/?appName=A2")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Na " + err.message));
+
+
 
 app.get("/", function(req, res) {
    res.render("login");
