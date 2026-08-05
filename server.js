7a4df38fec47b6f793149f2fb04502151530abfe
Ingrid Rangel Hernandez <rang0128@algonquinlive.com>
2026-08-05 17:23:16 -0400

    Sing up sends data to db and password is checked

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ mongoose.connect("mongodb+srv://ingridrh2005_db_user:e8rDFKeaMsecu6Ou@a2.ko4lkxu
   .catch((err) => console.log("Na " + err.message));
 
 
-
 app.get("/", function(req, res) {
    res.render("signup");
 });
@@ -29,6 +28,59 @@ app.post("/login", function (req, res) {
 });
 
 app.post("/signup", function(req, res) {
-    console.log(req.body.username + " " + req.body.password);
-
+    let errores = checkPassword(req.body.password, req.body.confirm_password,req);
+    if (errores.length > 0) {
+        console.log(errores);
+        res.render("signup", { errores: errores });
+        return;
+    }else {
+        let prueba = createUser(req.body.email, req.body.username, req.body.password, req.body.confirm_password);
+        console.log(prueba);
+        res.render("login");
+    }
 });
+
+
+//Funciones
+
+const userSchema = new mongoose.Schema({
+  email:  { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
+
+function checkPassword(password, confirmPassword,req) {
+    let errores = [];
+
+   if (password.length < 10 || password.length > 20){
+      errores.push("Password must be between 10 and 20 characters.");
+   }
+
+   let pas1Reg = /[a-z]/;
+   if (!pas1Reg.test(password)){
+      errores.push("Password must contain at least one lowercase character.");
+   }
+
+   let pas2Reg = /[A-Z]/;
+   if (!pas2Reg.test(password)) {
+      errores.push("Password must contain at least one uppercase character.");
+   }
+
+   let pas3Reg = /[0-9]/;
+   if (!pas3Reg.test(password)) {
+      errores.push("Password must contain at least one digit.");
+   }
+
+   if (password != confirmPassword) {
+      errores.push("Password and confirmation password don't match.");
+   }
+
+   return errores;
+}
+
+async function createUser(email, username, password, confirmPassword) {
+    let nuevo = await User.create({ email: email, username: username, password: password });
+    return nuevo;
+}
