279086b42cc229c4d1e2d2fa739abd4afbba683a
Ingrid Rangel Hernandez <rang0128@algonquinlive.com>
2026-08-08 20:46:53 -0400

    Fix4

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24
   },
-  store: MongoStore.create({
+ store: new MongoStore({
     mongoUrl: process.env.MONGODB_URI,
     ttl: 60 * 60 * 24
   })
